3d214c63f199841f90f855063ee09750132b4af3
Ivan Angonoa <ivan.X.Angonoa.-ND@disney.com>
2019-11-08 20:21:39 -0300

    map sprites and animation

--- Assets/Scripts/SpotCollider.cs
+++ Assets/Scripts/SpotCollider.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class SpotCollider : MonoBehaviour
 {
+    public GameObject bgIcon;
+    private Animator m_bgIconAnim;
+
+    private void Start()
+    {
+        m_bgIconAnim = bgIcon.GetComponent<Animator>();
+        m_bgIconAnim.updateMode = AnimatorUpdateMode.UnscaledTime;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+            m_bgIconAnim.SetBool("isActive", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            m_bgIconAnim.SetBool("isActive", false);
         }
     }
 }
